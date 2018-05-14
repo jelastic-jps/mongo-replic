e50a5cdeb18ebbc9e25e98f05f3b4c9260734334
dzotic9 <dzotic9@gmail.com>
2018-05-14 12:43:52 +0300

    Update configureScaling.js

--- scripts/configureScaling.js
+++ scripts/configureScaling.js
@@ -10,10 +10,9 @@ function isPrimary(nodeId) {
         "curl -fsSL \"https://raw.githubusercontent.com/dzotic9/mongo-replic/master/scripts/isMaster.sh\" -o /tmp/checkMaster.sh", 
         "/bin/bash /tmp/checkMaster.sh | grep ismaster | cut -c 15- | rev | cut -c 2- | rev"
     ];
-  java.lang.System.out.println("DEBUG - in isPrimary - nodeId -> " + nodeId);
+  
     oResp = exec(nodeId, cmd);
-    java.lang.System.out.println("DEBUG - in isPrimary - oResp -> " + oResp);
-    
+      
     if (!oResp || oResp.result != 0){
         return oResp;
     }
@@ -30,8 +29,7 @@ function addSlave(nodeId) {
             "curl -fsSL \"https://raw.githubusercontent.com/dzotic9/mongo-replic/master/scripts/addSlave.sh\" -o /tmp/addSlave.sh",
             "/bin/bash /tmp/addSlave.sh ${nodes.nosqldb.last.address}"
         ];
-    
-    java.lang.System.out.println("DEBUG - in isPrimary -> ");
+
     return exec(nodeId, cmd);
 }
 
@@ -42,13 +40,11 @@ function exec(nodeid, cmd) {
 }
 
 for (var i = 0, n = aNodes.length; i < n; i += 1) {
-    java.lang.System.out.println("DEBUG - in for - aNodes[i].nodeGroup -> " + aNodes[i].nodeGroup);
+
     if (aNodes[i].nodeGroup == "nosqldb") {
-        java.lang.System.out.println("DEBUG - in if -> ");
         if (isPrimary(aNodes[i].id) == "true") {
             oResp = addSlave(aNodes[i].id);
 
-            java.lang.System.out.println("DEBUG - in for - addSlave - oResp -> " + oResp);
             if (!oResp || oResp.result != 0){
                 return oResp;
             }

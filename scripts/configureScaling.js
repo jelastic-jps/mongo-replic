f0cb54ae5f39b4cabcb5de8fe090496f4d648d60
dzotic9 <dzotic9@gmail.com>
2018-05-15 16:02:35 +0300

    Update configureScaling.js

--- scripts/configureScaling.js
+++ scripts/configureScaling.js
@@ -1,6 +1,8 @@
 var sTargetAppid = getParam("TARGET_APPID"),
     oEnv = jelastic.env.control.GetEnvInfo(sTargetAppid, session),
+    nodesCount = "${nodes.nosqldb.length}",
     aNodes = oEnv.nodes,
+    slaveVote,
     oResp;
 
 function isPrimary(nodeId) {
@@ -24,14 +26,14 @@ function isPrimary(nodeId) {
     return oResp.out;
 }
 
-function addSlave(nodeId) {
+function addSlave(nodeId, priority) {
     var cmd,
         newIpAddress;
         
     newIpAddress = newNode || "${nodes.nosqldb.last.address}";
     cmd = [
             "curl -fsSL \"https://raw.githubusercontent.com/dzotic9/mongo-replic/master/scripts/addSlave.sh\" -o /tmp/addSlave.sh",
-            "/bin/bash /tmp/addSlave.sh " + newIpAddress
+            "/bin/bash /tmp/addSlave.sh " + newIpAddress + " " + priority
         ];
 
     return exec(nodeId, cmd);
@@ -47,8 +49,12 @@ for (var i = 0, n = aNodes.length; i < n; i += 1) {
 
     if (aNodes[i].nodeGroup == "nosqldb") {
         if (isPrimary(aNodes[i].id) == "true") {
-            oResp = addSlave(Number(aNodes[i].id));
+            
+            if ((Number(nodesCount) + Number("${event.response.nodes.length}")) >= 7) {
+                slaveVote = 0;
+            }
 
+            oResp = addSlave(Number(aNodes[i].id), slaveVote);
             if (!oResp || oResp.result != 0){
                 return oResp;
             }

8532f2bc5b1e0f5d2ad970a7bd1e3a05d5594d7f
dzotic9 <dzotic9@gmail.com>
2018-05-17 14:00:47 +0300

    Update checkPrimaryNode.js

--- scripts/checkPrimaryNode.js
+++ scripts/checkPrimaryNode.js
@@ -10,6 +10,7 @@ var NOSQL_GROUP = "nosqldb",
 java.lang.System.out.println("DEBUG - oNodes -> " +  oNodes);
 java.lang.System.out.println("DEBUG - nodesCount -> " +  nodesCount);
 for (i = 0, n = oNodes.length; i < n; i += 1) {
+    java.lang.System.out.println("DEBUG - oNodes[i].nodeGroup -> " +  oNodes[i].nodeGroup);
   if (oNodes[i].nodeGroup == NOSQL_GROUP) {
       java.lang.System.out.println("DEBUG - oNodes[i] -> " +  oNodes[i]);
     if (isPrimary(oNodes[i].id)) {

c4812b6d381c022d7d55e1eb14847081f5c20356
dzotic9 <dzotic9@gmail.com>
2018-05-17 11:34:33 +0300

    Create checkPrimaryNode.js

---
+++ scripts/checkPrimaryNode.js
@@ -0,0 +1,47 @@
+var NOSQL_GROUP = "nosqldb",
+    sTargetAppid = getParam("TARGET_APPID"),
+    oNodes = jelastic.env.control.GetEnvInfo(sTargetAppid, session).nodes,
+    nodesCount = "${nodes.nosqldb.length}",    
+    aNodes = oEnv.nodes,
+    slaveVote = 1,
+    oResp,
+    i, 
+    n;
+    
+for (i = 0, n = nodesCount; i < n; i += 1) {
+  if (oNodes[i].nodeGroup == NOSQL_GROUP) {
+    if (isPrimary(oNodes[i].id)) {
+      return {
+        result: 0,
+        onAfterReturn [next]: {
+          masterNodeIp: oNodes[i].address
+        }
+      }
+    }
+  }
+}
+
+function isPrimary(nodeId) {
+    var cmd;
+  
+    cmd = [
+        "curl -fsSL \"${baseUrl}scripts/isMaster.sh\" -o /tmp/checkMaster.sh", 
+        "/bin/bash /tmp/checkMaster.sh | grep ismaster | cut -c 15- | rev | cut -c 2- | rev"
+    ];
+  
+    oResp = exec(nodeId, cmd);
+      
+    if (!oResp || oResp.result != 0){
+        return oResp;
+    }
+  
+    if (oResp.responses) {
+        oResp = oResp.responses[0];
+    }
+    
+    return oResp.out;
+}
+
+return {
+  result: 0
+}

b0762d5c27cbc6b52b7f1a8b50c47c5b3803f432
dzotic9 <dzotic9@gmail.com>
2018-05-17 11:14:32 +0300

    Update initiate.sh

--- scripts/initiate.sh
+++ scripts/initiate.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-MASTER=${1}
-FIRST=${2}
-SECOND=${3}
-ARBITER=${4}
+MASTER_IP_ADDRESS=${1}
+FIRST_NODE_ADDRESS=${2}
+SECOND_NODE_ADDRESS=${3}
+ARBITER_NODE_ADDRESS=${4}
 EXEC_FUNCTION=${5}
 PORT=27017
 
-if [ "$MASTER" != "$FIRST" ]; then
-    SECOND=$FIRST;
-    MASTER=$SECOND;
+if [ "$MASTER_IP_ADDRESS" != "$FIRST_NODE_ADDRESS" ]; then
+    SECOND_NODE_ADDRESS=$FIRST_NODE_ADDRESS;
+    MASTER_IP_ADDRESS=$SECOND_NODE_ADDRESS;
 fi
 
 function initiate(){
 mongo << EOF
-    rs.initiate({
-      _id: "rs0",
-      members:[{
-        _id : 0,
-        host : "${MASTER}:${PORT}"
-      }]}
-    );
+rs.initiate({
+  _id: "rs0",
+  members:[{
+    _id : 0,
+    host : "${MASTER_IP_ADDRESS}:${PORT}"
+  }]}
+);
 EOF
 sleep 3;
 mongo << EOF
@@ -32,13 +32,13 @@ EOF
 
 function addSlave(){
 mongo << EOF
-    rs.add("${SECOND}");
+    rs.add("${SECOND_NODE_ADDRESS}");
 EOF
 }
 
 function addArbiter(){
-    mongo << EOF
-        rs.addArb("${ARBITER}");
+mongo << EOF
+    rs.addArb("${ARBITER_NODE_ADDRESS}");
 EOF
 }
 

a97f904b31cbd7bfdebaf56d6e860ef171f901ba
dzotic9 <dzotic9@gmail.com>
2018-05-18 10:06:00 +0300

    Update checkPrimaryNode.js

--- scripts/checkPrimaryNode.js
+++ scripts/checkPrimaryNode.js
@@ -1,25 +1,18 @@
 //nosqldbNodeGroup
 var sTargetAppid = getParam("TARGET_APPID"),
     oNodes = jelastic.env.control.GetEnvInfo(sTargetAppid, session).nodes,
-//     nodesCount = Number("${nodes.length}"),
     slaveVote = 1,
     obj,
     oResp,
     i, 
     n;
-jelastic.marketplace.console.WriteLog("oNodes.length -> " + oNodes.length);
 for (i = 0, n = oNodes.length; i < n; i += 1) {
-    jelastic.marketplace.console.WriteLog("DEBUG oNodes[i].nodeGroup -> " + oNodes[i].nodeGroup);
-    jelastic.marketplace.console.WriteLog("DEBUG oNodes[i] -> " + oNodes[i]);
   if (oNodes[i].nodeGroup == nosqldbNodeGroup) {
-      
-      jelastic.marketplace.console.WriteLog("checkPrimaryNode - oNodes[i].id -> " + oNodes[i].id);
-    if (isPrimary(oNodes[i].id)) {
-	    jelastic.marketplace.console.WriteLog("is Primary == true");
-jelastic.marketplace.console.WriteLog("in if -> ");
+      if (isPrimary(oNodes[i].id)) {
+
       oResp = {
-          result: 0,
-          onAfterReturn: []
+        result: 0,
+        onAfterReturn: []
       };
       obj = {}; obj[next] = {masterNodeId: oNodes[i].id}
       oResp.onAfterReturn.push(obj);
@@ -42,35 +35,21 @@ function isPrimary(nodeId) {
         "curl -fsSL \"https://raw.githubusercontent.com/dzotic9/mongo-replic/master/scripts/isMaster.sh\" -o /tmp/checkMaster.sh", 
         "/bin/bash /tmp/checkMaster.sh | grep ismaster | cut -c 15- | rev | cut -c 2- | rev && /bin/bash /tmp/checkMaster.sh | grep secondary | cut -c 16- | rev | cut -c 2- | rev"
     ];
-	
-	 
-    jelastic.marketplace.console.WriteLog("DEBUG checkPrimaryNode isPrimary nodeId -> " + nodeId);
-	jelastic.marketplace.console.WriteLog("DEBUG cmd -> " + cmd);
+
     oResp = exec(nodeId, cmd);
-      jelastic.marketplace.console.WriteLog("DEBUG oResp - exec -> " + oResp);
 
     if (!oResp || oResp.result != 0){
         return oResp;
     }
-	
-//     if (oResp.responses[1]) {
-//         if (oResp.responses[1].out == "") {
-// 	    return false;
-// 	}
-//     }
   
     if (oResp.responses) {
         oResp = oResp.responses[0];
 	    
 	if (oResp.out) {
-		jelastic.marketplace.console.WriteLog("DEBUG oResp.out -> " + oResp.out);
-		//jelastic.marketplace.console.WriteLog("DEBUG split -> " + oResp.out.split("\\n")[0]);
 	    aCmdResp = oResp.out.replace(/\n/, ",").split(",");
 	}
     }
 	
-	jelastic.marketplace.console.WriteLog("DEBUG oResp - aCmdResp[0] -> " + aCmdResp[0]);
-	jelastic.marketplace.console.WriteLog("DEBUG oResp - aCmdResp[1] -> " + aCmdResp[1]);
     if (aCmdResp[0] == "true" && aCmdResp[1] == "false") {
         return true;
     }

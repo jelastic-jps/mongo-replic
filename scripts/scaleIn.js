173cbe899e11459d5257955c7059eea7957a7e26
dzotic9 <dzotic9@gmail.com>
2018-06-01 11:02:26 +0300

    Update scaleIn.js

--- scripts/scaleIn.js
+++ scripts/scaleIn.js
@@ -75,8 +75,10 @@ function reconfigureRespSet() {
     oConfig = oConfig.replace(/NumberLong\(.*\)/g, "\"$&\"");
 	oConfig.replace(/ObjectId\(\"[0-9]\"\)/g, "");
     //oConfig = oConfig.replace(/ObjectId\(.*\)/g, "\\\"$&\\\"");
+	jelastic.marketplace.console.WriteLog("reconfigureRespSet -1111 -  oConfig ->" + oConfig.replace(/ObjectId\(.*\)/g, "\\\"$&\\\""));
+	jelastic.marketplace.console.WriteLog("reconfigureRespSet -2222 -  oConfig ->" + oConfig.replace(/ObjectId\(.*\)/g, "\\\"$&\\\"").replace(/[0-9a-z]{20,25}/, '\\\"$&\\\"'));
 	oConfig = oConfig.replace(/ObjectId\(.*\)/g, "\\\"$&\\\"").replace(/[0-9a-z]{20,25}/, '\\\"$&\\\"');
-	jelastic.marketplace.console.WriteLog("reconfigureRespSet -adter replace -  oConfig ->" + oConfig);
+	
     oConfig = oConfig.match(/{[\s\tA-Za-z\n\w:,.()\[\]{}"]+}/g);
     jelastic.marketplace.console.WriteLog("reconfigureRespSet -after match -  oConfig ->" + oConfig);
     oConfig = (oConfig.length > 0) ? oConfig[0] : oConfig;
